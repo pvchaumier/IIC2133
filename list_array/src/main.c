8621bc8a5ebe7b96d9c8dda26dfa03db707f9f99
Patricio López <lopezjuripatricio@gmail.com>
2015-11-09 09:45:25 -0300

    remove extra spaces on main

--- list_array/src/main.c
+++ list_array/src/main.c
@@ -5,7 +5,7 @@
 |   Comments:   IIC2133 - estructuras de datos - tarea1
 \******************************************************************************/
 
-#include <stdio.h> 
+#include <stdio.h>
 #include <stdlib.h>
 
 #include "array.h"
@@ -22,25 +22,25 @@ int main( int argc, char *argv[] )
 
     printf("Empty array\n");
     array_print(array);
-    
+
     printf("Array with 1, 2 and 3 added.\n");
     array_add(array, 1);
     array_add(array, 2);
     array_add(array, 3);
     array_print(array);
-    
+
     printf("Array delete the 3.\n");
     array_del(array, 3);
     array_print(array);
-   
- 
+
+
     printf("Looking for element at position number 1.\n");
     el = array_get(array, 1);
     printf("%d\n", el);
 
     printf("Looking for element at position number 2.\n");
     el = array_get(array, 2);
-    printf("%d\n", el); 
+    printf("%d\n", el);
 
     array_destroy(array);
 
@@ -53,26 +53,26 @@ int main( int argc, char *argv[] )
 
     printf("Empty list\n");
     list_print(list);
-    
+
     printf("LinkedList with 1, 2 and 3 added.\n");
     list_add(list, 1);
     list_add(list, 2);
     list_add(list, 3);
     list_print(list);
-    
+
     printf("LinkedList with 3 deleted.\n");
     list_del(list, 3);
     list_print(list);
- 
+
     printf("Looking for element at position number 1.\n");
     el = list_get(list, 1);
     printf("%d\n", el);
 
     printf("Looking for element at position number 2.\n");
     el = list_get(list, 2);
-    printf("%d\n", el); 
+    printf("%d\n", el);
 
     list_destroy(list);
- 
+
     return 0;
 }
